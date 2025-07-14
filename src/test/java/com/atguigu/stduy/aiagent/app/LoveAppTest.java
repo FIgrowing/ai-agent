ae5f8a9df00c3afbb73aac073bc7701c4305eff3
cokerzhou <cokerzhou@tencent.com>
2025-07-14 16:55:47 +0800

    测试持久化功能

---
+++ src/test/java/com/atguigu/stduy/aiagent/app/LoveAppTest.java
@@ -0,0 +1,25 @@
+package com.atguigu.stduy.aiagent.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Test
+    void doChatWithReport() {
+        LoveApp.LoveReport loveReport = loveApp.doChatWithReport("你好我叫周天赐，我想让另一半更喜欢我，该怎么做","001");
+    }
+
+    @Test
+    void doChat() {
+        String chat = loveApp.doChat("你知道我是谁吗？只需要回答我的名字即可，除此之外不要输出其他任何内容","003");
+    }
+}
