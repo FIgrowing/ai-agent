fec828faf95fe7846f6afacafd3f0b882e7c38cb
cokerzhou <cokerzhou@tencent.com>
2025-07-17 11:04:39 +0800

    更改apikey

---
+++ src/main/java/com/atguigu/stduy/aiagent/constant/AI.java
@@ -0,0 +1,6 @@
+package com.atguigu.stduy.aiagent.constant;
+
+public interface AI {
+    String API_KEY = "sk-bde8b8357b884953b55d743a7187de0e";
+    String serpApiKey = "7heFP5aUYns5RENP4qopL1Px";
+}
